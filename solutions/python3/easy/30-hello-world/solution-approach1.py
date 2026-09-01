6e0d034b9db41d94e13fb6e539511d70af4eeac5
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-01 16:05:53 +0530

    ✅ Day 0: Hello, World. [python3] — approach1

---
+++ solutions/python3/easy/30-hello-world/solution-approach1.py
@@ -0,0 +1,19 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
+# Problem     Day 0: Hello, World.
+# Difficulty  Easy
+# Subdomain   30 Days of Code
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-01, 04:05 p.m.
+# ──────────────────────────────────────────────────
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string)
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
