dc8a8ca4b41dcfaf97547737310a6bf95abc71d2
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 09:25:11 +0530

    ✅ Day 10: Binary Numbers [python3] — approach1

---
+++ solutions/python3/easy/30-binary-numbers/solution-approach1.py
@@ -0,0 +1,27 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/30-binary-numbers/problem?isFullScreen=true
+# Problem     Day 10: Binary Numbers
+# Difficulty  Easy
+# Subdomain   30 Days of Code
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 09:25 a.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    binary_str = bin(n)[2:]
+    ones_groups = binary_str.split('0')
+    max_consecutive_ones = max(len(group) for group in ones_groups)
+    print(max_consecutive_ones)
