57cada40cfa0cf0f71529abe48b6da2e174e9d85
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 11:09:36 +0530

    ✅ Python: Division [python3] — approach1

---
+++ solutions/python3/easy/python-division/solution-approach1.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Problem     Python: Division
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 11:09 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
