d5e83726af3dd65837ee110d3658dc16159db9b9
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 13:01:54 +0530

    ✅ Find Digits [python3] — approach1

---
+++ solutions/python3/easy/find-digits/solution-approach1.py
@@ -0,0 +1,49 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/find-digits/problem?isFullScreen=true
+# Problem     Find Digits
+# Difficulty  Easy
+# Subdomain   Implementation
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 01:01 p.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    count = 0
+    for digit_char in str(n):
+        digit = int(digit_char)
+        # Check if the digit is non-zero and divides n perfectly
+        if digit != 0 and n % digit == 0:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
