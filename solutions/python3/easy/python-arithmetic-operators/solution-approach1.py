ef253e5e5e81df00c70b52a39003db6382e5569e
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 11:09:47 +0530

    ✅ Arithmetic Operators [python3] — approach1

---
+++ solutions/python3/easy/python-arithmetic-operators/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Problem     Arithmetic Operators
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 11:09 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
