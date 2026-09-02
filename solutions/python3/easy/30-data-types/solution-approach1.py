78dbaf6287623701202561eba657c95be7058035
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 09:20:14 +0530

    ✅ Day 1: Data Types [python3] — approach1

---
+++ solutions/python3/easy/30-data-types/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/30-data-types/problem?isFullScreen=true
+# Problem     Day 1: Data Types
+# Difficulty  Easy
+# Subdomain   30 Days of Code
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 09:19 a.m.
+# ──────────────────────────────────────────────────
+
+
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+print(i + i2)
+print(round(d + d2, 1))
+print(s + s2)
