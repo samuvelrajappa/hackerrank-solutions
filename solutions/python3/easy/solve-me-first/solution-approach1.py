c2e6760382750039bc25f6a1bc1a584fff9714c5
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 11:58:51 +0530

    ✅ Solve Me First [python3] — approach1

---
+++ solutions/python3/easy/solve-me-first/solution-approach1.py
@@ -0,0 +1,21 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+# Problem     Solve Me First
+# Difficulty  Easy
+# Subdomain   Warmup
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 11:58 a.m.
+# ──────────────────────────────────────────────────
+
+
+
+def solveMeFirst(a,b):
+	return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
