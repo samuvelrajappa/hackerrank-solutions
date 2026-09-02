a69a2675e28c5d3bfa0a20939eb301c6b784f8f3
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 11:09:51 +0530

    ✅ Loops [python3] — approach1

---
+++ solutions/python3/easy/python-loops/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+# Problem     Loops
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 11:09 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i**2)
