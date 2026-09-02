d4e70dc9545625cd14fb3ba73d9f0f85e851ed02
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 13:03:02 +0530

    ✅ Find Digits [python3] — approach1

--- solutions/python3/easy/find-digits/solution-approach1.py
+++ solutions/python3/easy/find-digits/solution-approach1.py
@@ -6,7 +6,7 @@
 # Platform    HackerRank
 # Language    python3
 # Status      Accepted
-# Submitted   2026-09-02, 01:01 p.m.
+# Submitted   2026-09-02, 01:02 p.m.
 # ──────────────────────────────────────────────────
 
 #!/bin/python3
