0c686f861787119e8e70db3ca727bca2dbfb7b1b
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 11:12:49 +0530

    ✅ sWAP cASE [python3] — approach1

---
+++ solutions/python3/easy/swap-case/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
+# Problem     sWAP cASE
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 11:12 a.m.
+# ──────────────────────────────────────────────────
+
+def swap_case(s):
+    s  = s.swapcase()
+    return s
+
