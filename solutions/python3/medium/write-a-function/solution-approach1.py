0a89cce612947b8bc5fc380917022c077c06ef07
Mr. Samuvel Rajappa J <106525310+samuvelrajappa@users.noreply.github.com>
2026-09-02 11:09:40 +0530

    ✅ Write a function [python3] — approach1

---
+++ solutions/python3/medium/write-a-function/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
+# Problem     Write a function
+# Difficulty  Medium
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 11:09 a.m.
+# ──────────────────────────────────────────────────
+
+def is_leap(year):
+    leap = False
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        leap = True
+    # Write your logic here
+    return leap
+
